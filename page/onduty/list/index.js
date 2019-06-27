16453ea94b1f486a1ab914f2c67932f3c8b76b84
zoye1988 <nadt1988@126.com>
2019-06-27 20:59:14 +0800

    dhj

--- page/onduty/list/index.js
+++ page/onduty/list/index.js
@@ -5,7 +5,68 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    dutys:[
+      {
+        dptname:"支队机关",
+        duty:[
+          {
+            name:"带班领导",
+            val:"毕明"
+          },
+          {
+            name: "值班长",
+            val: "代红波"
+          },
+          {
+            name: "指挥长",
+            val: "陈乔"
+          },
+          {
+            name: "专职值班",
+            val: "段斌"
+          },
+          {
+            name: "作战值班",
+            val: "张昭华"
+          },
+        ],
+        person:{
+          fireman:10,
+          amatuer:12,
+          vacation:1,
+          dog:1
+        },
+        car:{
+          smallcar:5,
+          bigcar:12,
+          brokencar:1
+        }
+      },
+      {
+        dptname: "麒麟大队",
+        duty: [
+          {
+            name: "大队领导",
+            val: "赵琨"
+          },
+          {
+            name: "大队参谋",
+            val: "林瑾"
+          },
+        ],
+        person: {
+          fireman: 10,
+          amatuer: 12,
+          vacation: 1,
+          dog: 1
+        },
+        car: {
+          smallcar: 5,
+          bigcar: 12,
+          brokencar: 1
+        }
+      },
+    ]
   },
 
   /**
@@ -21,39 +82,10 @@ Page({
   onReady: function () {
 
   },
-
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
 
   },
 
